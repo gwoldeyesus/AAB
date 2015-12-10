0b5fbf18695e4ab2acc25f0e36edaf67afeb813d
Getaneh Woldeyesus <gwoldeyesus@hotmail.com>
2015-12-10 14:34:01 -0500

    1st Book Model

---
+++ AAB/AAB.Data/Model/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AAB.Data.Model
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Title is required!")]
+        [DataType(DataType.Text)]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Author is required!")]
+        public string WrittenBy { get; set; }
+        [Required(ErrorMessage = "Narrator is required!")]
+        public string NarratedBy { get; set; }
+        public TimeSpan Length { get; set; }
+        public string Description { get; set; }
+        public float Rate { get; set; }
+        public string SampleAudio { get; set; }
+        public string FullAudio { get; set; }
+        public string CoverArt { get; set; }
+    }
+}
