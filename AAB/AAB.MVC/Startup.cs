696223c1f546ae765398b48393fc06b35a5a222d
Getaneh Woldeyesus <gwoldeyesus@hotmail.com>
2015-12-10 14:18:31 -0500

    Out of the box commit

---
+++ AAB/AAB.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AAB.MVC.Startup))]
+namespace AAB.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
